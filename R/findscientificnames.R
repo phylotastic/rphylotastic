9d991ffb71aeed23e09de23196f041dc4c00a338
Brian O'Meara <omeara.brian@gmail.com>
2017-03-14 21:45:40 -0400

    updated with tests

--- R/findscientificnames.R
+++ R/findscientificnames.R
@@ -1,4 +1,5 @@
 #' Function to pull scientific names from web pages
+#'
 #' @param URL The URL to extract names from
 #' @param search.engine 1 to use TaxonFinder, 2 to use NetiNeti, 0 to use both
 #' @return A vector of scientific names
@@ -10,12 +11,13 @@ GetScientificNamesFromURL <- function(URL, search.engine=0) {
 }
 
 #' Function to pull scientific names from text
+#'
 #' @param text The text string to extract names from
 #' @param search.engine 1 to use TaxonFinder, 2 to use NetiNeti, 0 to use both
 #' @return A vector of scientific names
 #' @seealso \url{https://github.com/phylotastic/phylo_services_docs/blob/master/ServiceDescription/PhyloServicesDescription.md}
 #' @export
-GetScientificNamesFromText <- function(URL, search.engine=0) {
-  results <- jsonlite::fromJSON(base::URLencode(paste(GetBaseURL(), 'names_url?url=', URL, '&engine=', search.engine, sep="")))
+GetScientificNamesFromText <- function(text, search.engine=0) {
+  results <- jsonlite::fromJSON(utils::URLencode(paste(GetBaseURL(), 'names_url?url=', text, '&engine=', search.engine, sep="")))
   return(results$scientificNames)
 }
