3bea6c70ac07c9a6f0b92189565077fa3d60e6a1
Brian O'Meara <omeara.brian@gmail.com>
2017-04-21 19:06:06 -0400

    getting names works, though watch for homonyms or timing out

--- R/getspecies.R
+++ R/getspecies.R
@@ -1,10 +1,14 @@
 #' Get all species from a taxon
 #'
 #' @param taxon A taxon to get all species for
+#' @param filters A character vector of strings to exclude
 #' @return A vector of names
 #' @seealso \url{https://github.com/phylotastic/phylo_services_docs/blob/master/ServiceDescription/PhyloServicesDescription.md} or the rotl package, another interface to Open Tree of Life
 #' @export
-GetSpeciesFromTaxon <- function(taxon) {
+GetSpeciesFromTaxon <- function(taxon, filters=c("environmental", "sp\\.", "cf\\.")) {
   results <- jsonlite::fromJSON(paste(GetBaseURL(), 'ts/all_species?taxon=', utils::URLencode(taxon), sep=""))$species
+  for (i in sequence(length(filters))) {
+    results <- results[!grepl(filters[i], results)]
+  }
   return(results)
 }
