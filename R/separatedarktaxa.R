cd565e9622b420eb9feda9ab5d8c1dcfb32d47a7
Brian O'Meara <omeara.brian@gmail.com>
2017-05-06 12:08:27 -0500

    forgot to eliminate duplicates: we don't want to count if there are 15 seqs for each taxon

--- R/separatedarktaxa.R
+++ R/separatedarktaxa.R
@@ -6,7 +6,7 @@
 #' @seealso \url{https://github.com/phylotastic/phylo_services_docs/blob/master/ServiceDescription/PhyloServicesDescription.md} or the rotl package, another interface to Open Tree of Life
 #' @export
 SeparateDarkTaxaOToL <- function(taxon, filters=c("environmental", "sp\\.", "cf\\.")) {
-  results <- jsonlite::fromJSON(paste(GetBaseURL(), 'ts/all_species?taxon=', utils::URLencode(taxon), sep=""))$species
+  results <- unique(jsonlite::fromJSON(paste(GetBaseURL(), 'ts/all_species?taxon=', utils::URLencode(taxon), sep=""))$species)
   results.dark <- c()
   results.known <- results
   for (i in sequence(length(filters))) {
@@ -26,7 +26,7 @@ SeparateDarkTaxaGenbank <- function(taxon, filters=c("environmental", "sp\\.", "
   search.results <- rentrez::entrez_search("nuccore", term =paste0(taxon,"[Organism]"), use_history=TRUE)
   taxa.ids <- rentrez::entrez_link(dbfrom="nuccore", db="taxonomy", web_history=search.results$web_history)
   taxa.returns <- rentrez::entrez_summary(db="taxonomy", id=taxa.ids$links$nuccore_taxonomy)
-  results <- rentrez::extract_from_esummary(taxa.returns, "scientificname")
+  results <- unique(rentrez::extract_from_esummary(taxa.returns, "scientificname"))
   results.dark <- c()
   results.known <- results
   for (i in sequence(length(filters))) {
