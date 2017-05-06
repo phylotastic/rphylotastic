45a579dab35989aada9f4909a6459af1e8b13ca5
Brian O'Meara <omeara.brian@gmail.com>
2017-05-06 09:53:32 -0500

    help for new file

--- tests/testthat/test_getspecies.R
+++ tests/testthat/test_getspecies.R
@@ -2,3 +2,9 @@ test_that("Get species works", {
   results <- GetSpeciesFromTaxon("vulpes")
   expect_gte(length(results), 12)
 })
+
+
+test_that("Separating dark species works", {
+  results <- SeparateDarkTaxa("vulpes")
+  expect_gte(length(results), 12)
+})
