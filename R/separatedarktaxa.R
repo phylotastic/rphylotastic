95cbbb888f39b2d1c9cc5d78d2d3a744186cf1e0
Brian O'Meara <omeara.brian@gmail.com>
2017-05-06 11:37:08 -0500

    updated to handle dark taxa from NCBI

--- R/separatedarktaxa.R
+++ R/separatedarktaxa.R
@@ -1,12 +1,50 @@
-#' Separate dark from known taxa
+#' Separate dark from known taxa on OpenTree of Life
 #'
 #' @param taxon A taxon to get all species for
 #' @param filters A character vector of strings to exclude
 #' @return A vector of names
 #' @seealso \url{https://github.com/phylotastic/phylo_services_docs/blob/master/ServiceDescription/PhyloServicesDescription.md} or the rotl package, another interface to Open Tree of Life
 #' @export
-SeparateDarkTaxa <- function(taxon, filters=c("environmental", "sp\\.", "cf\\.")) {
+SeparateDarkTaxaOToL <- function(taxon, filters=c("environmental", "sp\\.", "cf\\.")) {
   results <- jsonlite::fromJSON(paste(GetBaseURL(), 'ts/all_species?taxon=', utils::URLencode(taxon), sep=""))$species
+  results.dark <- c()
+  results.known <- results
+  for (i in sequence(length(filters))) {
+    results.known <- results.known[!grepl(filters[i], results.known)]
+  }
+  results.dark <- setdiff(results, results.known)
+  return(list(dark=results.dark, known=results.known, fraction.dark = length(results.dark)/length(results)))
+}
+
+#' Separate dark from known taxa on another database
+#'
+#' @param taxon A taxon to get all species for
+#' @param filters A character vector of strings to exclude
+#' @param db See ?taxize::downstream for options
+#' @return A vector of names
+#' @export
+SeparateDarkTaxaDB <- function(taxon, filters=c("environmental", "sp\\.", "cf\\."), db="col") {
+  results <- jsonlite::fromJSON(paste(GetBaseURL(), 'ts/all_species?taxon=', utils::URLencode(taxon), sep=""))$species
+  results.dark <- c()
+  results.known <- results
+  for (i in sequence(length(filters))) {
+    results.known <- results.known[!grepl(filters[i], results.known)]
+  }
+  results.dark <- setdiff(results, results.known)
+  return(list(dark=results.dark, known=results.known, fraction.dark = length(results.dark)/length(results)))
+}
+
+#' Separate dark from known taxa on another database
+#'
+#' @param taxon A taxon to get all species for
+#' @param filters A character vector of strings to exclude
+#' @return A vector of names
+#' @export
+SeparateDarkTaxaGenbank <- function(taxon, filters=c("environmental", "sp\\.", "cf\\.")) {
+  search.results <- rentrez::entrez_search("nuccore", term =paste0(taxon,"[Organism]"), use_history=TRUE)
+  taxa.ids <- rentrez::entrez_link(dbfrom="nuccore", db="taxonomy", web_history=search.results$web_history)
+  taxa.returns <- rentrez::entrez_summary(db="taxonomy", id=taxa.ids$links$nuccore_taxonomy)
+  results <- rentrez::extract_from_esummary(taxa.returns, "scientificname")
   results.dark <- c()
   results.known <- results
   for (i in sequence(length(filters))) {
--- tests/testthat/test_getspecies.R
+++ tests/testthat/test_getspecies.R
@@ -4,7 +4,16 @@ test_that("Get species works", {
 })
 
 
-test_that("Separating dark species works", {
-  results <- SeparateDarkTaxa("vulpes")
+test_that("Separating dark species works for OToL", {
+  results <- SeparateDarkTaxaOToL("vulpes")
   expect_gte(length(results), 12)
+})
+
+
+test_that("Separating dark species works for NCBI", {
+  results <- SeparateDarkTaxaOToL("Poa")
+  expect_gte(length(results$dark), 5)
+  expect_gte(length(results$known), 5)
+  expect_gte(length(results$fraction.dark), 0)
+  expect_lte(length(results$fraction.dark), 1)
 })
