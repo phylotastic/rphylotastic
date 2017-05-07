5782e6811938e2d50e628117496aa910b35d67e6
Brian O'Meara <omeara.brian@gmail.com>
2017-05-07 13:52:17 -0500

    updated to allow over 10K species

--- R/separatedarktaxa.R
+++ R/separatedarktaxa.R
@@ -16,25 +16,25 @@ SeparateDarkTaxaOToL <- function(taxon, filters=c("environmental", "sp\\.", "cf\
   return(list(dark=results.dark, known=results.known, fraction.dark = length(results.dark)/length(results)))
 }
 
-#' Separate dark from known taxa on another database
-#'
-#' @param taxon A taxon to get all species for
-#' @param filters A character vector of strings to exclude
-#' @return A list containing a vector of dark names, a vector of known names, and fraction.dark
-#' @export
-SeparateDarkTaxaGenbankOLD <- function(taxon, filters=c("environmental", "sp\\.", "cf\\.")) {
-  search.results <- rentrez::entrez_search("nuccore", term =paste0(taxon,"[Organism]"), use_history=TRUE)
-  taxa.ids <- rentrez::entrez_link(dbfrom="nuccore", db="taxonomy", web_history=search.results$web_history)
-  taxa.returns <- rentrez::entrez_summary(db="taxonomy", id=taxa.ids$links$nuccore_taxonomy)
-  results <- unique(rentrez::extract_from_esummary(taxa.returns, "scientificname"))
-  results.dark <- c()
-  results.known <- results
-  for (i in sequence(length(filters))) {
-    results.known <- results.known[!grepl(filters[i], results.known)]
-  }
-  results.dark <- setdiff(results, results.known)
-  return(list(dark=results.dark, known=results.known, fraction.dark = length(results.dark)/length(results)))
-}
+# #' Separate dark from known taxa on another database
+# #'
+# #' @param taxon A taxon to get all species for
+# #' @param filters A character vector of strings to exclude
+# #' @return A list containing a vector of dark names, a vector of known names, and fraction.dark
+# #' @export
+# SeparateDarkTaxaGenbankOLD <- function(taxon, filters=c("environmental", "sp\\.", "cf\\.")) {
+#   search.results <- rentrez::entrez_search("nuccore", term =paste0(taxon,"[Organism]"), use_history=TRUE)
+#   taxa.ids <- rentrez::entrez_link(dbfrom="nuccore", db="taxonomy", web_history=search.results$web_history)
+#   taxa.returns <- rentrez::entrez_summary(db="taxonomy", id=taxa.ids$links$nuccore_taxonomy)
+#   results <- unique(rentrez::extract_from_esummary(taxa.returns, "scientificname"))
+#   results.dark <- c()
+#   results.known <- results
+#   for (i in sequence(length(filters))) {
+#     results.known <- results.known[!grepl(filters[i], results.known)]
+#   }
+#   results.dark <- setdiff(results, results.known)
+#   return(list(dark=results.dark, known=results.known, fraction.dark = length(results.dark)/length(results)))
+# }
 
 #' Separate dark from known taxa on another database
 #'
@@ -46,6 +46,12 @@ SeparateDarkTaxaGenbank <- function(taxon, filters=c("environmental", "sp\\.", "
   search.results <- rentrez::entrez_search("taxonomy", term =paste0(taxon,"[subtree] AND species[Rank] "), use_history=TRUE)
 #  search.fetch <- entrez_fetch(db="taxonomy", web_history=search.results$web_history, rettype="xml", parsed=TRUE)
   taxa.returns <- rentrez::entrez_summary(db="taxonomy", web_history=search.results$web_history, version=c("1.0"))
+  all.taxa.returns <- taxa.returns
+  loop.count <- 2
+  while(length(taxa.returns)==10000) {
+    taxa.returns <- rentrez::entrez_summary(db="taxonomy", web_history=search.results$web_history, version=c("1.0"),restart=(loop.count*10000)+1)
+    all.taxa.returns <- c(all.taxa.returns, taxa.returns)
+  }
   results <- unique(rentrez::extract_from_esummary(taxa.returns, "ScientificName"))
   results.dark <- c()
   results.known <- results
