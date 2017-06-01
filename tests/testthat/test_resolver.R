57e29eaa041c451729349daedc63540345efc582
Brian OMeara <omeara.brian@gmail.com>
2017-06-01 14:17:54 -0400

    adding fake species to the resolver unit test

--- tests/testthat/test_resolver.R
+++ tests/testthat/test_resolver.R
@@ -1,5 +1,5 @@
 test_that("Resolving names with GNR works", {
-  taxa <- c("Setophaga striata", "Setophaga megnolia", "Setophaga angilae", "Setophaga plumbea", "Setophaga virens")
+  taxa <- c("Fake Species", "Setophaga striata", "Setophaga megnolia", "Another fake species", "Setophaga angilae", "Setophaga plumbea", "Setophaga virens")
   expect_warning(taxa.new <- ResolveNamesWithGNR(taxa))
   expect_gte(length(taxa.new), 3)
 })
