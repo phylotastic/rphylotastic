6cd10dd022fe393036e20403504ea99fbf4502d2
Abu Saleh <abusalehmdtayeen@gmail.com>
2017-06-29 00:10:11 -0600

    Compare trees service added

---
+++ R/comparetrees.R
@@ -0,0 +1,17 @@
+#' Compare two phylogenetic trees
+#'
+#' @param trees A character vector of two newick trees
+#' @return A named list with are_same_tree property set to either TRUE or FALSE
+#' @seealso \url{https://github.com/phylotastic/phylo_services_docs/blob/master/ServiceDescription/PhyloServicesDescription.md}
+#' @export
+
+CompareTrees <- function(trees) {
+  url <- "http://phylo.cs.nmsu.edu:5006/phylotastic_ws/compare_trees"
+  body <- list(tree1_nwk = trees[1], tree2_nwk = trees[2])
+  response <- POST(url, body = body, encode = "json")
+  result <- content(response,"parsed")	
+  
+  return(result)
+}
+
+
