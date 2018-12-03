e3d7c6c5f6c507807c20a54d15a46a5cad525a93
Brian O'Meara <omeara.brian@gmail.com>
2018-12-03 13:18:21 -0500

    death to port numbers

--- R/resolvescientificnames.R
+++ R/resolvescientificnames.R
@@ -16,3 +16,28 @@ taxa_resolve_names_with_gnr <- function(taxa) {
   }
   return(final.names)
 }
+
+#' Convert common names to scientific names
+#'
+#' @param taxa The vector of names
+#' @param service Which service to use: NCBI, ITIS, or TROPICOS
+#' @param multiple  If TRUE, then the service will return multiple matches (if available) for each common name in the input list.
+#' @return A vector of correct names. THE ORDER MAY NOT CORRESPOND TO YOUR INPUT ORDER.
+#' @seealso taxize package for name resolution in general and its sci2comm function.
+#' @examples
+#' taxa <- c("blue whale", "swordfish","killer whale")
+#' common <- taxa_convert_common_to_scientific(taxa)
+#' print(common)
+#' @export
+taxa_convert_common_to_scientific <- function(taxa, service="NCBI", multiple=FALSE) {
+  taxa.string <- utils::URLencode(paste(taxa, collapse="|"))
+  service <- tolower(service)
+  results <- jsonlite::fromJSON(paste0(rphylotastic:::get_base_url(), 'cs/', service, '/get_scientific_names?commonnames=', taxa.string))
+  final.names <- c()
+  final.names <- unlist(lapply(results$resolvedNames$matched_results, "[[", "matched_name"))
+
+  if(length(final.names) < length(taxa)) {
+      warning("Fewer names were found than were given; missing taxa were dropped.")
+  }
+  return(final.names)
+}
