0dbcfb799bce5496f67793a0211c791734f9573f
Brian O'Meara <omeara.brian@gmail.com>
2018-12-03 13:25:55 -0500

    no, ports still exist

--- R/base.R
+++ R/base.R
@@ -1,5 +1,5 @@
 #' Get Phylotastic base URL
 #' return The URL for the phylotastic server
 get_base_url <- function() {
-  return("http://phylo.cs.nmsu.edu/phylotastic_ws/")
+  return("http://phylo.cs.nmsu.edu:5004/phylotastic_ws/")
 }
